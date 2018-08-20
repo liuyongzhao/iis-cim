cd1b06bb7b0f9048070cae9c8529f757726edfbb
qiyuqi <qiyuqi@supcon.com>
2018-08-20 15:43:46 +0800

    :sparkles: add new feature for active the config from command-line with "-Dspring.profiles.active=" or "--spring.profiles.active="

---
+++ src/main/java/com/supcon/soft/iis/cim/utils/CommandUtils.java
@@ -0,0 +1,38 @@
+package com.supcon.soft.iis.cim.utils;
+
+
+public class CommandUtils {
+    public static final String GLOBAL_SYS_PROFILE_KEY = "profile";
+
+    public static String getAndSetSpringProfile(String[] args){
+        String profile = null;
+        if(args != null && args.length > 0){
+            for(String arg : args){
+                String p = parseSpringProfile(arg);
+                if (p != null){
+                    profile = p;
+                    System.setProperty(GLOBAL_SYS_PROFILE_KEY, profile);
+                }
+            }
+        }
+        if(profile == null){
+            System.setProperty(GLOBAL_SYS_PROFILE_KEY, Env.dev.name());
+        }
+        return profile;
+    }
+
+    private static String parseSpringProfile(String arg){
+        String result = parseSpringProfile(arg, "-Dspring.profiles.active=");
+        if(result == null){
+            result = parseSpringProfile(arg, "--spring.profiles.active=");
+        }
+        return result;
+    }
+
+    private static String parseSpringProfile(String arg, String captain){
+        if(!arg.startsWith(captain)){
+            return null;
+        }
+        return arg.substring(captain.length());
+    }
+}
---
+++ src/main/java/com/supcon/soft/iis/cim/utils/Env.java
@@ -0,0 +1,5 @@
+package com.supcon.soft.iis.cim.utils;
+
+public enum Env {
+    dev, test, product
+}
